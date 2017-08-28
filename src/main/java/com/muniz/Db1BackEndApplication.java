86176653c97814481e6e4597f74c97d59c6b1b58
mmuniz75 <muniz_marcelo@hotmail.com>
2017-08-28 17:32:20 -0300

    change to post

--- src/main/java/com/muniz/Db1BackEndApplication.java
+++ src/main/java/com/muniz/Db1BackEndApplication.java
@@ -3,9 +3,9 @@ package com.muniz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,9 +16,19 @@ public class Db1BackEndApplication {
 		SpringApplication.run(Db1BackEndApplication.class, args);
 	}
 	
+	/*
 	@CrossOrigin
 	@RequestMapping(value="/checkPassword/{password}",method=RequestMethod.GET)
 	public Integer passwordMeter(@PathVariable String password){
+		PasswordMeter passwordMeter = new PasswordMeter();
+		//return passwordMeter.scorePasswordDummy(password);
+		return passwordMeter.scorePassword(password);
+	}
+	*/
+	
+	@CrossOrigin
+	@RequestMapping(value="/checkPassword",method=RequestMethod.POST)
+	public Integer passwordMeter(@RequestParam("password") String password){
 		PasswordMeter passwordMeter = new PasswordMeter();
 		//return passwordMeter.scorePasswordDummy(password);
 		return passwordMeter.scorePassword(password);
